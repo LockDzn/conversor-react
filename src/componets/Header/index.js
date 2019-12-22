fee888276973683668df666efd3ad41ae8099167
Ryan <ryanssouza00@gmail.com>
2019-12-22 18:00:53 -0300

    :small_orange_diamond: fix twitter link

--- src/componets/Header/index.js
+++ src/componets/Header/index.js
@@ -7,7 +7,7 @@ import './style.css';
 const Header = () => (
     <header id="main-header">
         <a href="https://github.com/lockdzn/conversor-react"><img src={githublogo} alt="GitHub" className="g"/></a>
-        <a href="https://twitter.com/lockdzn"><img src={twitterlogo} alt="Twitter" className="l"/></a>
+        <a href="https://twitter.com/lockdzn_"><img src={twitterlogo} alt="Twitter" className="l"/></a>
     </header>
 );
 
